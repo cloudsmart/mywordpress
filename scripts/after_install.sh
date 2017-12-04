28ceea4db893561792101904b7110c6f76c1900a
EC2 Default User <ec2-user@ip-10-10-0-100.ec2.internal>
2017-12-04 19:01:11 +0000

    Added CodeDeploy Scripts

---
+++ scripts/after_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+chmod -R 755 /var/www/html
+
+mkdir /var/www/html/wp-content
+
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-6a062b23.efs.us-east-1.amazonaws.com:/ /var/www/html/wp-content
+
+cp -rn /var/www/html/wp_content_original/* /var/www/html/wp_content
+
+chown -R apache:apache wp_content
